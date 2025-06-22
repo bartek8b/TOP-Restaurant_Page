9795325e414bb0144150d3e5f5118a307ce9ef73
bartek8b <163062773+bartek8b@users.noreply.github.com>
2025-06-22 20:35:46 +0200

    Handle team.js module

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import "./style.css";
 import { creator } from "./creator.js";
 import { restaurantImg, imgAlt as homeImgAlt, text as homeText } from "./home.js";
+import { teamImg, imgAlt as teamImgAlt, text as teamText } from "./team.js";
 
 console.log('creator:', creator);
 
 const homeBtn = document.querySelector('.homeBtn');
+const teamBtn = document.querySelector('.teamBtn');
 
 homeBtn.addEventListener('click', () => creator(restaurantImg, homeImgAlt, homeText));
+teamBtn.addEventListener('click', () => creator(teamImg, teamImgAlt, teamText));
 
 window.onload = () => creator(restaurantImg, homeImgAlt, homeText);
---
+++ src/team.js
@@ -0,0 +1,14 @@
+import teamImg from "./assets/team.png";
+
+const imgAlt = 'picture of our team';
+
+const text = `We are glad to introduce our dummyselves to you:
+<br><br>
+<ul>
+    <li>Dummy Enterpreneur - CEO</li>
+    <li>Dummy Cook - Chief</li>
+    <li>Dummy waiter - Head of waiters dpt</li>
+</ul>
+`;
+
+export { teamImg, imgAlt, text }
