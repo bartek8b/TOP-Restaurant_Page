287f338f0d9adce5737044bd43b15740f0fe5b9c
bartek8b <163062773+bartek8b@users.noreply.github.com>
2025-06-22 20:51:49 +0200

    Handle menu.js module

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import "./style.css";
 import { creator } from "./creator.js";
 import { restaurantImg, imgAlt as homeImgAlt, text as homeText } from "./home.js";
 import { teamImg, imgAlt as teamImgAlt, text as teamText } from "./team.js";
+import { menuImg, imgAlt as menuImgAlt, text as menuText } from "./menu.js";
 
 console.log('creator:', creator);
 
 const homeBtn = document.querySelector('.homeBtn');
 const teamBtn = document.querySelector('.teamBtn');
+const menuBtn = document.querySelector('.menuBtn');
 
 homeBtn.addEventListener('click', () => creator(restaurantImg, homeImgAlt, homeText));
 teamBtn.addEventListener('click', () => creator(teamImg, teamImgAlt, teamText));
+menuBtn.addEventListener('click', () => creator(menuImg, menuImgAlt, menuText));
 
 window.onload = () => creator(restaurantImg, homeImgAlt, homeText);
---
+++ src/menu.js
@@ -0,0 +1,18 @@
+import menuImg from "./assets/menu.png";
+
+const imgAlt = 'picture of menu booklet';
+
+const text = `Enjoy our dummy dishes:
+<br><br>
+<ul>
+    <li><p>Dummy starter</p><p>&euro; 3.00</p></li>
+    <li><p>Dummy soup</p><p>&euro; 7.00</p></li>
+    <li><p>Dummy main dish</p><p>&euro; 10.00</p></li>
+    <li><p>Dummy dessert</p><p>&euro; 7.00</p></li>
+    <li><p>Dummy coffe/tea dish</p><p>&euro; 3.00</p></li>
+    <li><p>Dummy beer</p><p>&euro; 4.00</p></li>
+    <li><p>Dummy child dish</p><p>&euro; 8.00</p></li>
+</ul>
+`;
+
+export { menuImg, imgAlt, text }
