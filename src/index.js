fe07a13af7672eca54502f900bce6829bab637ad
bartek8b <163062773+bartek8b@users.noreply.github.com>
2025-06-22 19:21:10 +0200

    Add home.js module

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
 import "./style.css";
 import { creator } from "./creator.js";
+import { imgSrc as homeImgSrc } from "./home.js";
+import { imgAlt as homeImgAlt } from "./home.js";
+import { text as homeText } from "./home.js";
